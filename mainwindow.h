621e7a930d2fe167e58365d3f2426aa411894359
Rakhimov Abdurasul <rakhimovabdurasul@gmail.com>
2018-11-04 18:14:02 +0300

    Add files via upload

--- mainwindow.h
+++ mainwindow.h
@@ -34,9 +34,13 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
-    QLineSeries *series0, *series1, *series2, *series3, *series4, *series5, *series6, *series7, *series00, *series01, *series02, *series03;
-    QChart *chart0, *chart1, *chart2, *chart3, *chart4, *chart5, *chart6, *chart7;
-    QChartView *chartView0, *chartView1, *chartView2, *chartView3, *chartView4, *chartView5, *chartView6, *chartView7;
+    QLineSeries *series0, *series1, *series2, *series3, *series4, *series5,
+                *series6, *series7, *series8, *series9, *series10,
+                *series00, *series01, *series02, *series03;
+    QChart *chart0, *chart1, *chart2, *chart3, *chart4, *chart5,
+            *chart6, *chart7, *chart8, *chart9, *chart10;
+    QChartView *chartView0, *chartView1, *chartView2, *chartView3, *chartView4, *chartView5,
+                *chartView6, *chartView7, *chartView8, *chartView9, *chartView10;
     QTableWidget *table;
     QMessageBox *msg;
 private slots:
